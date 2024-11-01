29d65e26b08c792024932058b98e11d8329d7e42
Lydos <lydossky@gmail.com>
2024-11-01 21:21:14 +0300

    display current conditions

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const API_KEY = "L65HRL2TSN56KX7HYS324LYP6";
 const API_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
-let weatherData;
 
 let country = "london";
 let unit = "metric";
 
-const WeatherData = function (description, currentCondition, days) {
-  return { description, currentCondition, days };
+const WeatherData = function (description, currentConditions, days) {
+  return { description, currentConditions, days };
 };
 
 const WeatherModule = (function () {
@@ -32,8 +31,40 @@ const WeatherModule = (function () {
   return { getWeatherData };
 })();
 
-const UI = (function () {})();
+const UI = (function () {
+  function drawCurrentWeather(currentWeather, unit) {
+    let symbol;
+    if (unit === "metric") symbol = "°C";
+    if (unit === "us") symbol = "°F";
+    const currentWeatherContainer = document.querySelector("#current-weather");
+    const card = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const small = document.createElement("small");
+    const feelslike = document.createElement("small");
+    const humidity = document.createElement("small");
+    const uvindex = document.createElement("small");
+    const dewPoint = document.createElement("small");
+
+    h2.innerText = `${currentWeather.temp} ${symbol}`;
+    small.innerText = currentWeather.conditions;
+    feelslike.innerText = `Feelslike: ${currentWeather.feelslike} ${symbol}`;
+    humidity.innerText = `Humidity: ${currentWeather.humidity}%`;
+    dewPoint.innerText = `Dew Point: ${currentWeather.dew}`;
+    uvindex.innerText = `UV: ${currentWeather.uvindex}`;
+
+    card.appendChild(h2);
+    card.appendChild(small);
+    card.appendChild(feelslike);
+    card.appendChild(humidity);
+    card.appendChild(dewPoint);
+    card.appendChild(uvindex);
+
+    currentWeatherContainer.appendChild(card);
+  }
+
+  return { drawCurrentWeather };
+})();
 
 WeatherModule.getWeatherData(country, unit).then((weather) =>
-  console.log(weather),
+  UI.drawCurrentWeather(weather.currentConditions, unit),
 );
