3341102776863d2f7a7716cb6a1f66ea793e28d1
Lydos <lydossky@gmail.com>
2024-11-03 11:58:46 +0300

    card look finished

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import MOCK_DATA from "./data.js";
+
 const API_KEY = "L65HRL2TSN56KX7HYS324LYP6";
 const API_URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/`;
 
@@ -33,17 +35,33 @@ const WeatherModule = (function () {
 
 const UI = (function () {
   function drawCurrentWeather(currentWeather, unit) {
+    const currentWeatherContainer = document.querySelector("#current-weather");
+    const weatherCard = createWeatherCard(
+      currentWeather.currentConditions,
+      unit,
+    );
+
+    currentWeatherContainer.appendChild(weatherCard);
+  }
+
+  function createWeatherCard(currentWeather, unit) {
     let symbol;
     if (unit === "metric") symbol = "°C";
     if (unit === "us") symbol = "°F";
-    const currentWeatherContainer = document.querySelector("#current-weather");
+
     const card = document.createElement("div");
+    const cardLeftContainer = document.createElement("div");
+    const cardRightContainer = document.createElement("div");
     const h2 = document.createElement("h2");
     const small = document.createElement("small");
     const feelslike = document.createElement("small");
     const humidity = document.createElement("small");
     const uvindex = document.createElement("small");
     const dewPoint = document.createElement("small");
+
+    card.classList.add("card");
+    cardLeftContainer.classList.add("card-left-container");
+    cardRightContainer.classList.add("card-right-container");
 
     h2.innerText = `${currentWeather.temp} ${symbol}`;
     small.innerText = currentWeather.conditions;
@@ -52,19 +70,25 @@ const UI = (function () {
     dewPoint.innerText = `Dew Point: ${currentWeather.dew}`;
     uvindex.innerText = `UV: ${currentWeather.uvindex}`;
 
-    card.appendChild(h2);
-    card.appendChild(small);
-    card.appendChild(feelslike);
-    card.appendChild(humidity);
-    card.appendChild(dewPoint);
-    card.appendChild(uvindex);
+    cardLeftContainer.appendChild(h2);
+    cardLeftContainer.appendChild(small);
+    cardRightContainer.appendChild(feelslike);
+    cardRightContainer.appendChild(humidity);
+    cardRightContainer.appendChild(dewPoint);
+    cardRightContainer.appendChild(uvindex);
 
-    currentWeatherContainer.appendChild(card);
+    card.appendChild(cardLeftContainer);
+    card.appendChild(cardRightContainer);
+
+    return card;
   }
 
   return { drawCurrentWeather };
 })();
 
-WeatherModule.getWeatherData(country, unit).then((weather) =>
-  UI.drawCurrentWeather(weather.currentConditions, unit),
-);
+// WeatherModule.getWeatherData(country, unit).then((weather) =>
+//   UI.drawCurrentWeather(weather.currentConditions, unit),
+// );
+console.log(MOCK_DATA);
+
+UI.drawCurrentWeather(MOCK_DATA, unit);
